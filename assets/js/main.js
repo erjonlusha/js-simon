43b06d5249fd7fe84ebe3725710fd990feeb287a
Erjon <erjonlusha@gmail.com>
2020-12-01 18:44:55 +0100

    Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,14 +50,58 @@ while (numCasual.length !==5) {
   }
 }
 
-alert("random 5 numeri casuali " + numCasual);
+alert("Ricordati questi numeri " + numCasual);
 
 
 //Dopo 30 secondi l’utente deve inserire
-setTimeout(myFunction, 3000);
-function myFunction() {
-    var i = 0, numUser = [];
-    do{
-        numUser = Number(prompt("Inserisci un numero di quelli usciti prima"));
-    } while (numUser.length == 5) ;
+var numUser=[];
+var corretti=[];
+setTimeout(myFunction1, 3000);
+function myFunction1() {
+
+    var clock = setInterval(myFunction, 3000);
+    function myFunction() {
+        for (var i = 0; numUser.length < 5; i++) {
+            var userNumero = Number(prompt("Inserisci un numero di quelli usciti prima"));
+            while (inArray(numUser, userNumero)) {
+                alert("Il numero è stato già inserito")
+                var userNumero = Number(prompt("Inserisci un numero di quelli usciti prima"));    
+            }
+            numUser.push(userNumero);
+        }
+
+        if (numUser.length == 5) { 
+            clearInterval(clock);
+        }
+
+        // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+        // var numCasual= [23,45,74];
+        // var numUser= [23,45,74];
+        for (var i=0;i<numUser.length;i++) {
+            rv = false;
+            for (var j=0;j<numCasual.length;j++) {
+                rv = rv || numUser[i]==numCasual[j];
+                if ( numUser[i]==numCasual[j]){
+                    corretti[i]=numUser[i];
+                }
+            }
+            if(rv) {
+                alert("I numeri corretti sono " + corretti[i]);
+            }
+        }
+        // console.log("DENTRO FUNZIONE");        
+        // console.log("lunghezza numero casuale " + numCasual.length);
+        // console.log("numero casuale " + numCasual);
+        // console.log("lunghezza numero utente " + numUser.length);
+        // console.log("numero utente " + numUser);
+    }
+
+    
+    alert("I numeri corretti sono " + corretti.length);
 }
+// console.log("FUORI FUNZIONE" );
+// console.log("lunghezza numero casuale " + numCasual.length);
+ console.log("numero casuale " + numCasual);
+// console.log("lunghezza numero utente " + numUser.length);
+// console.log("numero utente " + numUser);
+
